15ed759e6f20fdef4bd6970afd140859234e2ce3
stecyuk8899 <my_name@nuwm.edu.ua>
2025-06-25 16:51:14 +0300

    create project calculator(very simple) for doctest

---
+++ calculations.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return float(a + b)
+
+
+def subtract(a, b):
+    return float(a - b)
+
+
+def multiply(a, b):
+    return float(a * b)
+
+
+def divide(a, b):
+    return float(a / b)
+
+
+a = 5
+b = 3
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
